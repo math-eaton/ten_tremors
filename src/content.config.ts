c10a8fbf3566b39860422b7ca495beceace3dd96
math-eaton <mjh2241@columbia.edu>
2025-02-11 18:10:44 -0500

    release -> artist linking + init artefacts coll

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,22 @@ import { file } from 'astro/loaders';
 
 // Load artists.json
 const artistsCollection = defineCollection({
-  loader: file("src/content/artists.json"),
+  loader: file("src/content/collections/artists.json"),
 });
 
 // Load releases.json
 const releasesCollection = defineCollection({
-  loader: file("src/content/releases.json"),
+  loader: file("src/content/collections/releases.json"),
 });
+
+// Load artefacts.json
+const artefactsCollection = defineCollection({
+  loader: file("src/content/collections/artefacts.json"),
+});
+
 
 export const collections = {
   artists: artistsCollection,
   releases: releasesCollection,
+  artefacts: artefactsCollection,
 };
--- src/content/artefacts/horse-shirt.md
+++
@@ -1,7 +0,0 @@
----
-name: "Limited Edition Shirt"
-price: "$25"
-image: "/images/shirt-one.jpg"
-description: "A high-quality cotton t-shirt featuring the Ten Tremors logo."
-purchaseLink: "https://store.tentremors.com/shirt-one"
----
